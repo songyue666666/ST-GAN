f6270a4a0f142016ca7dc57f19b6307e8f7ab0f2
songyue666666 <49330545+songyue666666@users.noreply.github.com>
2021-11-04 16:11:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -37,34 +37,3 @@ class Test:
         threshold = 0.58
         # threshold = self.threshold
         helpers.plotting1(self.id, "Anomaly score (GDScore)", pred, threshold, lab)
-
-        # x = []
-        # y = []
-        # maxx = 0
-        # maxy = 0
-        # for j in range(100):
-        #     threshold1 = j / 100
-        #     predict = []
-        #     for i in range(len(pred)):
-        #         if pred[i] < threshold1:
-        #             predict.append(1)
-        #         else:
-        #             predict.append(0)
-        #     pre, recall, F1 = helpers.indicator(predict, lab)
-        #     if F1 > 0.31:
-        #         F1 = F1 + 0.6
-        #     x.append(threshold1)
-        #     y.append(F1)
-        #     if maxy < F1:
-        #         maxx = threshold1
-        #         maxy = F1
-        # plt.figure()
-        # plt.plot(x, y, linewidth=2)
-        # plt.ylim(0, 1)
-        # plt.plot(maxx, maxy, marker = "o",markersize=10)
-        # # plt.vlines(0.58, 0, 1)
-        # font = {"family": "Times New Roman", "size": 18}
-        # plt.xlabel("Threshold", font)
-        # plt.ylabel("F1 of ", font)
-        # plt.tick_params(labelsize=15)
-        # plt.show()
