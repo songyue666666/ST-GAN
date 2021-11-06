e6ae6af9141fe868389685ad3baa920101998bf9
songyue666666 <49330545+songyue666666@users.noreply.github.com>
2021-11-06 15:08:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,32 +8,14 @@ logger = logging.getLogger('crgan')
 
 class Test:
     def __init__(self, data, config):
-        self.threshold = 0
         self.config = config
         self.data = data
         self.id = data.id
 
     def test(self):
-        y_hat = np.load("results/" + self.config.use_id + "/y_hat/y_hat.npy")
-        # cos_simi = np.load("results/" + self.config.use_id + "/y_hat/cos_simi.npy")
-        label = self.data.test_label
+        GDscore = np.load("results/" + self.config.use_id + "/y_hat/GDscore.npy")
+        label = self.data.test_label # read from csv
         helpers.load_para(self, "results/" + self.config.use_id + "/models/testparas")
-        pred = []
-        lab = []
-        for j in range(len(y_hat)//30):
-            pred.append(y_hat[(j+1)*30-1])
-            lab.append(label[(j+1)*30-1])
-        pred = y_hat.tolist()
-        lab = [0 for i in range(27927)]
-        for i in range(0, 5120):
-            lab[i] = 1
-        for i in range(11343, 11381):
-            lab[i] = 1
-        for i in range(12208, 12250):
-            lab[i] = 1
-        for i in range(13178, 13210):
-            lab[i] = 1
-        # pred = (np.ones(len(pred)) - np.array(pred)).tolist()
-        # threshold = 0.58
         threshold = self.threshold
-        helpers.plotting1(self.id, "Anomaly score (GDScore)", pred, threshold, lab)
+        helpers.plotting1(self.id, "Anomaly score (GDScore)", GDscore, threshold, label)
+        helpers.indicator(GDscore, label)
